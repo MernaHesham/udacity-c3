d8a29e700c790c8ec6624e0c4000d5a375af0fc4
Merna Hesham <mernahesham97@yahoo.com>
2021-07-13 13:40:51 +0200

    added screenshots

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ import os
 import numpy as np
 app = FastAPI()
 
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 cat_features = [
     "workclass",
     "education",

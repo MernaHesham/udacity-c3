96a77507d6f2b6c8f169ee0bd255309d72271204
Merna Hesham <mernahesham97@yahoo.com>
2021-07-13 13:02:26 +0200

    added screenshots

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from joblib import load
 from model import inference
 from data import process_data
@@ -42,8 +42,21 @@ class features(BaseModel):
     class Config:
         # to change the col names into the formated params
         alias_generator = to_format
-        schema_extra = {
-            "example": {
+        
+        
+
+@app.get("/")
+async def welcome():
+    return {"msg": f"Hello there!"}
+
+
+@app.post("/predict")
+def inference( X: features = Body(default= True,
+    examples={
+        "normal": {
+            "summary": "A normal example",
+            "description": "A **normal** item works correctly.",
+            "value": {
                 "age": 50,
                 "workclass": "Self-emp-not-inc",
                 "flngt": 83311,
@@ -59,22 +72,36 @@ class features(BaseModel):
                 "hours-per-week": 13,
                 "native-country": "United-States"
             },
-            
+        },
+        "converted": {
+            "summary": "An example with converted data",
+            "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+            "value": {
+                "age": 51,
+                "workclass": "Self-emp-not-inc",
+                "flngt": 83311,
+                "education": "Bachelors",
+                "education-num": 13,
+                "marital-status": "Married-civ-spouse",
+                "occupation": "Exec-managerial",
+                "relationship": "Husband",
+                "race": "White",
+                "sex": "Male",
+                "capital-gain": 0,
+                "capital-loss": 0,
+                "hours-per-week": 13,
+                "native-country": "United-States"
+            },
         }
-        
+    },
 
-@app.get("/")
-async def welcome():
-    return {"msg": f"Hello there!"}
-
-
-@app.post("/predict")
-def inference( X: features):
-    data_dict = features
+)):
+    
+    data_dict = X.dict()
     model = load('model.joblib')
     encoder = load('encoder.joblib')
     lb = load('lb.joblib')
-    data = pd.DataFrame.from_dict(features, orient='index')
+    data = pd.DataFrame(data_dict, index=[0])
     X_test, y_test, encoder,lb = process_data(
     data, categorical_features=cat_features, training=False, encoder= encoder, lb= lb
     )
